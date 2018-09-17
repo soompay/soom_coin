0e4235566dd9bf5b047508f781df0d6ce210f258
soul790916 <samyoung79@soompay.net>
2018-09-17 17:17:50 +0900

    WalletViewController add

---
+++ SOOM_APP/ViewController/WalletViewController.h
@@ -0,0 +1,13 @@
+//
+//  WalletViewController.h
+//  SOOM_APP
+//
+//  Created by samyoung79 on 2018. 9. 17..
+//  Copyright © 2018년 samyoung79. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WalletViewController : UIViewController
+
+@end
