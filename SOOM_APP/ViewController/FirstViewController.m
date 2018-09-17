1a5b304e2877d92a3d6cd026bd260ed53ea7fcf7
soul790916 <samyoung79@soompay.net>
2018-09-17 14:47:37 +0900

    push

--- SOOM_APP/ViewController/FirstViewController.m
+++ SOOM_APP/ViewController/FirstViewController.m
@@ -16,6 +16,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
